0ac9d3f729b442dba70230ab4f0336928c17f7d9
PRINCE PAL <268012144+Prince151915@users.noreply.github.com>
2026-09-16 20:29:49 +0530

    Time: 3 ms (68.90%) | Memory: 26.8 MB (14.07%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool is_palindrome(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+      
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+               return is_palindrome(s,left+1,right) ||is_palindrome(s,left,right-1);
+            }
+             else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
