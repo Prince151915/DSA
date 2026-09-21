208e6aadc8cf44b5afb3e8805d09b0902dbf33ed
PRINCE PAL <268012144+Prince151915@users.noreply.github.com>
2026-09-21 21:43:27 +0530

    Time: 3 ms (9.15%) | Memory: 10.7 MB (42.21%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
+        // create map for analysing element having unique frequency in map.
         map<int,int>mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
+        // initialize sum as 0.
         int sum=0;
         for(auto &i : mp){
             if(i.second==1){
